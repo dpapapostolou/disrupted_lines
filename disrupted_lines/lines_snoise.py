2873ee1f09041176808370e3e1f1511dd3a23712
dpapapostolou <dpapapostolou@gmail.com>
2019-06-10 21:20:58 +0200

    experiments with snoise2

--- disrupted_lines/lines_snoise.py
+++ disrupted_lines/lines_snoise.py
@@ -4,20 +4,21 @@ import time
 from noise import pnoise2, snoise2
 
 tk = Tk()
-canvas = Canvas(tk, width=3000, height=3000, bg='black')
+canvas = Canvas(tk, width=800, height=800, bg='black')
 canvas.pack()
 
 x = 10
 y = 10
+num = 20
 
 coordinates = [(x, y)]
 
 
 def first_line(coordinates, colour):
     xi, yi = coordinates[0][0], coordinates[0][1]
-    while xi < 2990:
+    while xi < 790:
         xf = xi + 20
-        yf = yi + snoise2(xi, yi, 6)
+        yf = yi * (1 + snoise2(yi, xi, 6)/num)
         canvas.create_line(xi, yi, xf, yf, fill=colour)
         tk.update_idletasks()
         tk.update()
@@ -29,23 +30,28 @@ def first_line(coordinates, colour):
 
 
 def repeat_lines(coordinates, colour):
+
     octave = 4
+    line_width = 1 #random.randint(1, 5)
+
     first_line(coordinates, colour)
-    while coordinates[-1][1] < 1500:
-        # seed_value += float(0.1)
-        line_width = random.randint(1, 5)
-        new_coordinates = [(coordinates[0][0], coordinates[0][1] + 10 + snoise2(coordinates[0][0],
-                                                                                coordinates[0][1], octave))]
+    num = 50
+
+
+    while coordinates[-1][1] < 790:
+        new_coordinates = [(coordinates[0][0], 20 + coordinates[0][1]*( 1 + snoise2(coordinates[0][1], coordinates[0][0], octave)/num))]
         for point in coordinates:
             xi = point[0]
-            yi = new_coordinates[-1][1]
+            yi = point[1]
             xf = xi + 20
-            yf = yi + snoise2(xi, yi, octave)
+            yf = yi * (1 + snoise2(yi, xi, octave)/num)
             canvas.create_line(xi, yi, xf, yf, fill=colour, width=line_width)
             tk.update_idletasks()
             tk.update()
             # time.sleep(0.001)
             new_coordinates.append((xf, yf))
+
+        # import ipdb; ipdb.set_trace()
         coordinates = new_coordinates
 
 
